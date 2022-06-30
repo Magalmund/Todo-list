854f0e81596feec42814484bd87fd2f3eabb173f
Magalmund <70335621+Magalmund@users.noreply.github.com>
2022-06-30 19:13:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+i//SELECTORS
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//EVENT LISTENER
+
+todoButton.addEventListener('click', addTodo);
+
+
+//FUNCTION
+function addTodo(event) {
+	//Prevent form from submitting
+	event.preventDefault();
+	//Todo DIV
+	const todoDiv = document.createElement("div");
+	todoDiv.classList.add("todo");
+	//Create LI
+	const newTodo = document.createElement('li');
+	newTodo.innerText = 'hey';
+	newTodo.classList.add('todo-item');
+	todoDiv.appendChild(newTodo);
+	//Check mark button
+	const completedButton = document.createElement('button');
+	completedButton.innerText = '<i class="fas fa-check"></i>';
+	completedButton.classList.add("complete-btn");
+	todoDiv.appendChild(completedButton);
+	//Chech trash button
+	const trashButton = document.createElement('button');
+	trashButton.innerText = '<i class="fas fa-trash"></i>';
+	trashButton.classList.add("complete-btn");
+	todoDiv.appendChild(trashButton);
+	//Append to list
+	todoList.appendChild(todoDiv)
+}
